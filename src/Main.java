1c9ad3f51e5097f04afa428e3db389295e8ad6ea
toker-777 <toker_777@bk.ru>
2024-09-21 10:17:08 +0300

    Conditional operator aligned the code

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
         System.out.println("Задание 2");
 
         byte degrees = 15;
-        System.out.println(" На улице " + degrees + " градуса");
+        System.out.println(" На улице " + degrees + " градусов(а)");
         if (degrees <= 5) {
             System.out.println(" На улице холодно, нужно надеть шапку ");
         } else {
@@ -36,7 +36,7 @@ public class Main {
     public static void task3() {
         System.out.println("Задание 3");
 
-        short speed = 60;
+        short speed = 140;
         System.out.println(" Если ты едешь со скоростью - " + speed + " км/ч ");
         if (speed > 60) {
             System.out.println(" Дружище, ты превышаешь скорость ");
@@ -49,11 +49,11 @@ public class Main {
         System.out.println("Задание 4");
 
         byte age = 18;
-        System.out.println(" Если тебе - " + age + " лет ");
         boolean needToGoKinderGarden = ((age >= 2) && (age <= 6));
         boolean needToGoSchool = ((age >= 7) && (age <= 17));
         boolean needToGoUniversity = (age >= 18 && age <= 24);
         boolean needToWork = (age > 24);
+        System.out.println(" Если тебе - " + age + " лет ");
         if (needToGoKinderGarden) {
             System.out.println(" Тебе нужно идти в детский сад ");
         } else {
@@ -80,16 +80,16 @@ public class Main {
         System.out.println("Задание 5");
 
         byte age = 5;
-        System.out.println("Если тебе - " + age + " лет ");
         boolean uDoNot = (age < 5);
         boolean mustBeAdults = ((age >= 5) && (age < 14));
         boolean uCan = (age >= 14);
+        System.out.println("Если тебе - " + age + " лет.  Можно ли кататься на аттракционе? ");
         if (uDoNot) {
-            System.out.println(" Ты не можешь кататься на аттракционе ");
+            System.out.println("Ты не можешь кататься на аттракционе ");
         } else if (mustBeAdults) {
-            System.out.println(" Ты можешь кататься на аттракционе, но в присутствии взрослых ");
+            System.out.println("Ты можешь кататься на аттракционе, но в присутствии взрослых ");
         } else if (uCan) {
-            System.out.println(" Ты можешь кататься на аттракционе ");
+            System.out.println("Ты можешь кататься на аттракционе ");
         }
     }
 
@@ -99,9 +99,10 @@ public class Main {
         byte passengers = 89;
         byte totalSeatsInTheCarriage = 102;
         byte seatsInTheCarriage = 60;
-        System.out.println("Если хочет зайти - " + passengers + " пассажира(ов) в вагон ");
         boolean stillFreeSeats = (seatsInTheCarriage > passengers);
         boolean stillFreeStandingPlaces = ((seatsInTheCarriage <= passengers) && (passengers < totalSeatsInTheCarriage));
+        System.out.println("Всего в вагоне - " + totalSeatsInTheCarriage + " мест(а). Из них - " + seatsInTheCarriage + " сидячих мест(а).");
+        System.out.println("Если в вагон зайдет - " + passengers + " пассажира(ов)");
         if (stillFreeSeats) {
             System.out.println("То останется - " + (seatsInTheCarriage - passengers) + " свободных сидячих мест. И - " + (totalSeatsInTheCarriage - seatsInTheCarriage) + " свободных стоячих мест(а).");
         } else if (stillFreeStandingPlaces) {
@@ -114,17 +115,18 @@ public class Main {
     public static void task7() {
         System.out.println("Задание 7");
 
-        int one = 33567;
+        int one = 33568;
         int two = 23456;
         int three = 33567;
         boolean oneBiggest = ((one > two) && (one > three));
         boolean twoBiggest = ((one < two) && (two > three));
+        System.out.println("Сравним три числа: первое " + one + " второе " + two + " третье " + three + " какое из них больше?");
         if (oneBiggest) {
-            System.out.println(" Самое большое число " + one);
+            System.out.println("Правильно, самое большое число " + one);
         } else if (twoBiggest) {
-            System.out.println(" Самое большое число " + two);
+            System.out.println("Правильно, самое большое число " + two);
         } else {
-            System.out.println(" Самое большое число " + three);
+            System.out.println("Правильно, самое большое число " + three);
         }
     }
 }
