af709de314638c55f0c6072999d24b29526bd13e
toker-777 <toker_777@bk.ru>
2024-09-21 10:23:53 +0300

    Conditional operator

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-//        task1();
-//        task2();
-//        task3();
-//        task4();
+        task1();
+        task2();
+        task3();
+        task4();
         task5();
-//        task6();
-//        task7();
+        task6();
+        task7();
     }
 
     public static void task1() {
